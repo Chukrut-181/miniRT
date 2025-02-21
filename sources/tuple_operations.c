1577aa9d4f7baa147d35f99c3b8c1176c66949fc
Ignacio Churruca Fernandez De Tejada <igchurru@c4r7s2.42urduliz.com>
2025-02-21 10:58:33 +0100

    Added vector operations. Norme OK.

--- sources/tuple_operations.c
+++ sources/tuple_operations.c
@@ -6,7 +6,7 @@
 /*   By: igchurru <igchurru@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/20 15:36:49 by igchurru          #+#    #+#             */
-/*   Updated: 2025/02/20 15:42:24 by igchurru         ###   ########.fr       */
+/*   Updated: 2025/02/21 09:59:06 by igchurru         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,15 +34,26 @@ t_tuple	*ft_substract_tuples(t_tuple *tuple1, t_tuple *tuple2)
 	return (substraction);
 }
 
-t_tuple	*ft_scale_tuple(t_tuple *tuple, float scalar)
+t_tuple	*ft_multiply_tuple(t_tuple *tuple, float scalar)
 {
-	t_tuple	*scaled;
+	t_tuple	*multiplied;
 
-	scaled->x = tuple->x * scalar;
-	scaled->y = tuple->y * scalar;
-	scaled->z = tuple->z * scalar;
-	scaled->w = tuple->w;
-	return (scaled);
+	multiplied->x = tuple->x * scalar;
+	multiplied->y = tuple->y * scalar;
+	multiplied->z = tuple->z * scalar;
+	multiplied->w = tuple->w;
+	return (multiplied);
+}
+
+t_tuple	*ft_divide_tuple(t_tuple *tuple, float scalar)
+{
+	t_tuple	*divided;
+
+	divided->x = tuple->x / scalar;
+	divided->y = tuple->y / scalar;
+	divided->z = tuple->z / scalar;
+	divided->w = tuple->w;
+	return (divided);
 }
 
 t_tuple	*ft_negate_tuple(t_tuple *tuple)
---
+++ sources/vector_operations.c
@@ -0,0 +1,62 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   vector_operations.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: igchurru <igchurru@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/21 10:04:30 by igchurru          #+#    #+#             */
+/*   Updated: 2025/02/21 10:58:16 by igchurru         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/minirt.h"
+
+/* The distance represented by a vector is called its magnitude, or length.
+It’s how far you would travel in a straight line if you were to walk from
+one end of the vector to the other. */
+float	ft_calculate_magnitude(t_tuple *v)
+{
+	return (sqrtf(v->x * v->x + v->y * v->y + v->z * v->z + v->w * v->w));
+}
+
+/* Vectors with magnitudes of 1 are called unit vectors. Normalization is the
+process of taking an arbitrary vector and converting it into a unit vector. */
+void	ft_normalize(t_tuple *v)
+{
+	float	mag;
+
+	mag = ft_calculate_magnitude(v);
+	v->x *= mag;
+	v->y *= mag;
+	v->z *= mag;
+}
+
+/* The dot product takes two vectors and returns a scalar value.
+The smaller the dot product, the larger the angle between the vectors.
+Given two unit vectors, a dot product of 1 means the vectors are identical,
+and a dot product of -1 means they point in opposite directions.
+Specifically, and again if the two vectors are unit vectors, the dot product
+is actually the cosine of the angle between them. */
+float	ft_dot_product(t_tuple *v1, t_tuple *v2)
+{
+	return (v1->x * v2->x + v1->y * v2->y + v1->z * v2->z + v1->w * v2->w);
+}
+
+/* This function 'creates' a new vector that is perpendicular to both the
+original vectors. Note that this is specifically testing vectors, not tuples.
+Also, note that if you change the order of the operands, you change the
+direction of the resulting vector.Order matters!
+X cross Y gives you Z, but Y cross X gives you -Z! */
+t_tuple	*ft_cross_product(t_tuple *v1, t_tuple *v2)
+{
+	t_tuple	*cross_vector;
+
+	if (v1->w != 0 || v2->w != 0)
+		return (NULL);
+	cross_vector->x = v1->y * v2->z - v1->z * v2->y;
+	cross_vector->y = v1->z * v2->x - v1->x * v2->z;
+	cross_vector->z = v1->x * v2->y - v1->y * v2->x;
+	cross_vector->w = 0;
+	return (cross_vector);
+}
