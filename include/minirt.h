7de0fa8aff010d17cbc5a9adff6028e09184142c
Ignacio Churruca Fernandez De Tejada <igchurru@c4r7s2.42urduliz.com>
2025-02-24 10:04:45 +0100

    Updates header file minirt.h

--- include/minirt.h
+++ include/minirt.h
@@ -6,7 +6,7 @@
 /*   By: igchurru <igchurru@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/19 10:28:29 by igchurru          #+#    #+#             */
-/*   Updated: 2025/02/21 11:28:37 by igchurru         ###   ########.fr       */
+/*   Updated: 2025/02/24 10:04:35 by igchurru         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,6 +32,24 @@ int		main(int argc, char **argv);
 
 //	MINIRT
 int	ft_minirt(void);
+
+//	TUPLE CREATION
+t_tuple	ft_create_point(float x, float y, float z);
+t_tuple	ft_create_vector(float x, float y, float z);
+int		ft_check_equality(t_tuple *tuple1, t_tuple *tuple2);
+
+//	TUPLE OPERATIONS
+t_tuple	ft_add_tuples(t_tuple *tuple1, t_tuple *tuple2);
+t_tuple	ft_substract_tuples(t_tuple *tuple1, t_tuple *tuple2);
+t_tuple	ft_multiply_tuple(t_tuple *tuple, float scalar);
+t_tuple	ft_divide_tuple(t_tuple *tuple, float scalar);
+t_tuple	ft_negate_tuple(t_tuple *tuple);
+
+//	VECTOR OPERATIONS
+float	ft_calculate_magnitude(t_tuple *v);
+void	ft_normalize(t_tuple *v);
+float	ft_dot_product(t_tuple *v1, t_tuple *v2);
+t_tuple	ft_cross_product(t_tuple *v1, t_tuple *v2);
 
 //	HOOKS
 void	ft_handle_key(mlx_key_data_t keydata, void *param);
