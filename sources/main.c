f4a844b3d2473d1a37d7e56113d7a507f9031da3
eandres <eandres@student.42urduliz.com>
2025-02-26 17:13:33 +0100

    translation

--- sources/main.c
+++ sources/main.c
@@ -6,7 +6,7 @@
 /*   By: igchurru <igchurru@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/19 11:26:57 by igchurru          #+#    #+#             */
-/*   Updated: 2025/02/26 14:59:08 by eandres          ###   ########.fr       */
+/*   Updated: 2025/02/26 16:48:15 by eandres          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,15 @@
 
 int	main(int argc, char **argv)
 {
+	t_tuple *tuple = malloc(sizeof(t_tuple) * 1);
+
+	tuple->x = 2;
+	tuple->z = 2;
+	tuple->y = 2;
 	(void)argv;
 	if (argc != 2)
 		ft_error_exit("Error\nUsage: ./miniRT <arg1>", 1);
 	else
-		translation();
+		translation(tuple);
 	return (0);
 }
--- sources/matrix_transformation.c
+++ sources/matrix_transformation.c
@@ -1,51 +1,53 @@
 #include "../include/minirt.h"
 
-static void	ft_print_4x4(t_4x4 *matrix)
-{
-	int i = 0;
-	int j;
-
-	while (i < 4)
-	{
-		j = 0;
-		while (j < 4)
-		{
-			printf("%f ", matrix->data[i][j]);
-			j++;
-		}
-		printf("\n");
-		i++;
-	}
-}
-
+//static void	ft_print_4x4(t_4x4 *matrix)
+//{
+//	int i = 0;
+//	int j;
+//
+//	while (i < 4)
+//	{
+//		j = 0;
+//		while (j < 4)
+//		{
+//			printf("%f ", matrix->data[i][j]);
+//			j++;
+//		}
+//		printf("\n");
+//		i++;
+//	}
+//}
+//
 static t_4x4	*ft_create_identity_matrix()
 {
-	static t_4x4	matrix;
+	t_4x4	*matrix = malloc(sizeof(t_4x4) * 1);
 
-	matrix.data[0][0] = 1.0;
-	matrix.data[0][1] = 0.0;
-	matrix.data[0][2] = 0.0;
-	matrix.data[0][3] = 0.0;
-	matrix.data[1][0] = 0.0;
-	matrix.data[1][1] = 1.0;
-	matrix.data[1][2] = 0.0;
-	matrix.data[1][3] = 0.0;
-	matrix.data[2][0] = 0.0;
-	matrix.data[2][1] = 0.0;
-	matrix.data[2][2] = 1.0;
-	matrix.data[2][3] = 0.0;
-	matrix.data[3][0] = 0.0;
-	matrix.data[3][1] = 0.0;
-	matrix.data[3][2] = 0.0;
-	matrix.data[3][3] = 1.0;
-	return (&matrix);
+	matrix->data[0][0] = 1.0;
+	matrix->data[0][1] = 0.0;
+	matrix->data[0][2] = 0.0;
+	matrix->data[0][3] = 0.0;
+	matrix->data[1][0] = 0.0;
+	matrix->data[1][1] = 1.0;
+	matrix->data[1][2] = 0.0;
+	matrix->data[1][3] = 0.0;
+	matrix->data[2][0] = 0.0;
+	matrix->data[2][1] = 0.0;
+	matrix->data[2][2] = 1.0;
+	matrix->data[2][3] = 0.0;
+	matrix->data[3][0] = 0.0;
+	matrix->data[3][1] = 0.0;
+	matrix->data[3][2] = 0.0;
+	matrix->data[3][3] = 1.0;
+	return (matrix);
 }
 
-t_4x4	*translation()
+t_4x4	*translation(t_tuple *tuple)
 {
 	t_4x4	*identity_matrix;
 
 	identity_matrix = ft_create_identity_matrix();
-	ft_print_4x4(identity_matrix);
+	identity_matrix->data[0][3] = tuple->x;
+	identity_matrix->data[1][3] = tuple->y;
+	identity_matrix->data[2][3] = tuple->z;
 	return (identity_matrix);
 }
