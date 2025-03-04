17691c5baf7b14d6112e33bbcb850080c9d4c2ef
Ignacio Churruca Fernandez De Tejada <igchurru@c4r7s2.42urduliz.com>
2025-03-04 15:58:34 +0100

    Shearing matrix is working.

--- sources/matrix_transformation.c
+++ sources/matrix_transformation.c
@@ -45,3 +45,17 @@ t_4x4	*create_scalation_mx(t_tuple *tuple)
 	scalation_mx->data[2][2] = tuple->z;
 	return (scalation_mx);
 }
+
+t_4x4	*ft_create_shearing_mx(t_tuple *tuple)
+{
+	t_4x4	*shearing_mx;
+
+	shearing_mx = ft_create_identity_matrix();
+	shearing_mx->data[0][1] = tuple->x + tuple->y;
+	shearing_mx->data[0][2] = tuple->x + tuple->z;
+	shearing_mx->data[1][0] = tuple->y + tuple->x;
+	shearing_mx->data[1][3] = tuple->y + tuple->z;
+	shearing_mx->data[2][0] = tuple->z + tuple->x;
+	shearing_mx->data[2][1] = tuple->z + tuple->y;
+	return (shearing_mx);
+}
--- sources/test_clock.c
+++ sources/test_clock.c
@@ -6,7 +6,7 @@
 /*   By: igchurru <igchurru@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/27 13:47:46 by igchurru          #+#    #+#             */
-/*   Updated: 2025/03/04 12:48:27 by igchurru         ###   ########.fr       */
+/*   Updated: 2025/03/04 15:56:20 by igchurru         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,6 +36,7 @@ void	ft_draw_clock(mlx_image_t *image)
 	t_tuple		hour_position;
 	t_4x4		*rotation_matrix;
 	t_4x4		*translation_matrix;
+	t_4x4		*shearing_matrix;
 	t_4x4		*transformation_matrix;
 	int			hour;
 	double		angle;
@@ -48,6 +49,7 @@ void	ft_draw_clock(mlx_image_t *image)
 	radius = 500;
 	translation_matrix = create_translation_mx
 		(ft_create_point(image->width / 2, image->height / 2, 0));
+	shearing_matrix = ft_create_shearing_mx(ft_create_point(-0.3, 0, 0));
 	twelve_oclock = *ft_create_point(0, -radius, 0);
 	ft_draw_large_pixel(image, center_x, center_y, 10);
 	hour = 0;
@@ -55,16 +57,14 @@ void	ft_draw_clock(mlx_image_t *image)
 	{
 		angle = (float)hour * (2 * M_PI / 12);
 		rotation_matrix = rotation_z(angle);
-		transformation_matrix = ft_multiply_matrices
-			(translation_matrix, rotation_matrix);
-		hour_position = *ft_multiply_mat_and_tuple
-			(transformation_matrix, &twelve_oclock);
+		transformation_matrix = ft_multiply_matrices(translation_matrix, ft_multiply_matrices(shearing_matrix, rotation_matrix));
+		hour_position = *ft_multiply_mat_and_tuple(transformation_matrix, &twelve_oclock);
 		printf("X = %9.3f, Y = %9.3f\n", hour_position.x, hour_position.y);
-		ft_draw_large_pixel
-			(image, (int)hour_position.x, (int)hour_position.y, 15);
+		ft_draw_large_pixel (image, (int)hour_position.x, (int)hour_position.y, 15);
 		free(rotation_matrix);
 		free(transformation_matrix);
 		hour++;
 	}
 	free(translation_matrix);
+	free(shearing_matrix);
 }
