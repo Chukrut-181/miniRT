a22b56cf38d347100bcac75dc38c6622293cad55
Ignacio Churruca Fernandez De Tejada <igchurru@c4r7s2.42urduliz.com>
2025-03-10 11:53:38 +0100

    Sphere renderization OK, moving on!

--- sources/test_sphere.c
+++ sources/test_sphere.c
@@ -6,11 +6,32 @@
 /*   By: igchurru <igchurru@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/07 13:57:06 by igchurru          #+#    #+#             */
-/*   Updated: 2025/03/10 10:36:03 by igchurru         ###   ########.fr       */
+/*   Updated: 2025/03/10 11:53:13 by igchurru         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minirt.h"
+
+static t_ray ft_transform_ray(t_ray ray, t_4x4 *m)
+{
+    t_ray transformed;
+
+    transformed.origin = *ft_multiply_mat_and_tuple(m, &ray.origin);
+    transformed.direction = *ft_multiply_mat_and_tuple(m, &ray.direction);
+    return (transformed);
+}
+
+
+static t_4x4 *ft_transformation_test(void)
+{
+	t_4x4	*transform_test;
+
+	transform_test =  NULL;
+	transform_test = ft_find_inverse(create_scalation_mx(ft_create_point(0.25, 1, 1)));
+	if (!transform_test)
+		return (write(1, "matriz caca\n", 12), NULL);
+	return (transform_test);
+}
 
 void	ft_render_sphere(mlx_image_t *image)
 {
@@ -23,8 +44,8 @@ void	ft_render_sphere(mlx_image_t *image)
 	int			x;
 	int			y;
 
-
 	sphere = *ft_create_sphere(*ft_create_point(0, 0, 0), 10);
+	//ray_origin = *ft_multiply_mat_and_tuple(ft_transformation_test(), ft_create_point(0, 0, -100));
 	ray_origin = *ft_create_point(0, 0, -100);
 	xs_list = NULL;
 	x = 0;
@@ -36,6 +57,7 @@ void	ft_render_sphere(mlx_image_t *image)
 			direction = *ft_create_vector(x - 1200, 900 - y, 5000);
 			ft_normalize(&direction);
 			ray = ft_create_ray(ray_origin, direction);
+			ray = ft_transform_ray(ray, ft_transformation_test());
 			xs_list = ft_intersection(ray, sphere, xs_list);
 			while (xs_list)
 			{
