ae243dfc081a6e38c8e6f0569b70061821e42bbe
eandres <eandres@student.42urduliz.com>
2025-03-12 15:41:33 +0100

    update light and shading

--- sources/light_shading.c
+++ sources/light_shading.c
@@ -1,6 +1,18 @@
 #include "../include/minirt.h"
 
-// testear estas dos funciones (eliminar estor posteriormente)
+// testear estas tres funciones (eliminar estor posteriormente)
+
+t_tuple	ft_create_material(float x, float y, float z)
+{
+	t_material m;
+
+	m.color = ft_create_point(x, y, z);
+	m.ambient = 0,1;
+	m.diffuse = 0.9;
+	m.specular = 0.9;
+	m.shininess = 200.0;
+	return (m);
+}
 
 t_tuple	*normal_at(t_sphere *sphere, t_tuple *world_point)
 {
@@ -19,5 +31,47 @@ t_tuple	*normal_at(t_sphere *sphere, t_tuple *world_point)
 
 t_tuple	*reflect(t_tuple *in, t_tuple *normal)
 {
-	
+	t_tuple	*str1;
+	t_tuple	*str;
+	t_tuple	*result;
+
+	str1 = ft_multiply_tuple(normal, 2);
+	str = ft_cross_product(str1, ft_dot_product(in, normal));
+	result = ft_substract_tuples(in, str);
+	return (result);
+}
+
+t_tuple	*point_light(t_tuple position, t_tuple intensity)
+{
+	t_light *light;
+
+	light.position = position;
+	light.intensity = intensity;
+	return (light);
+}
+
+t_tuple	lighting(t_material mat, t_light light, t_tuple point, t_tuple eyev, t_tuple normalv)
+{
+	t_tuple	*effective_color;
+	t_tuple	*lightv;
+	t_tuple	*ambient;
+	float	light_dot_normal;
+	float	var;
+
+	effective_color = ft_cross_product(mat.color, light.intensity);
+	lightv = ft_substract_tuples(light.position, point);
+	ft_normalize(lightv);
+	ambient = ft_multiply_tuple(effective_color, material.ambient);
+	light_dot_normal = ft_dot_product(lightv, normalv);
+	if (light_dot_normal < 0)
+	{
+		material.diffuse = 0;
+		material.specular = 0;
+	}
+	else
+	{
+		var = material.diffuse * light_dot_normal;
+		material.diffuse = ft_multiply_tuple(effective_color, var);
+		
+	}
 }
