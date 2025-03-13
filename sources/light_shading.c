a8618439a5b79146fc84db800843cd647ba40e15
eandres <eandres@student.42urduliz.com>
2025-03-13 13:02:27 +0100

    update ligth

--- sources/light_shading.c
+++ sources/light_shading.c
@@ -7,7 +7,7 @@ t_tuple	ft_create_material(float x, float y, float z)
 	t_material m;
 
 	m.color = ft_create_point(x, y, z);
-	m.ambient = 0,1;
+	m.ambient = 0.1;
 	m.diffuse = 0.9;
 	m.specular = 0.9;
 	m.shininess = 200.0;
@@ -21,32 +21,43 @@ t_tuple	*normal_at(t_sphere *sphere, t_tuple *world_point)
 	t_tuple *world_normal;
 	t_4x4	*inverse_sphere;
 
-	inverse_sphere = ft_find_inverse(sphere.transform);
-	object_point = ft_cross_product(inverse_sphere, world_point);
+	inverse_sphere = ft_find_inverse(sphere->transform);
+	object_point = ft_multiply_mat_and_tuple(inverse_sphere, world_point);
 	object_normal = ft_substract_tuples(object_point, ft_create_point(0, 0, 0));
-	world_normal = ft_transpose(ft_cross_product(inverse_sphere, object_normal));
-	world_normal.w = 0;
-	return (ft_normalize(world_normal));
+
+	t_4x4	*transpose_inverse = ft_transpose(inverse_sphere);
+	world_normal = ft_multiply_mat_and_tuple(transpose_inverse, object_normal);
+	world_normal->w = 0;
+	ft_normalize(world_normal)
+
+	free(inverse_sphere);
+	free(transpose_inverse);
+	free(object_point);
+	free(object_normal);
+	return (world_normal);
 }
 
 t_tuple	*reflect(t_tuple *in, t_tuple *normal)
 {
-	t_tuple	*str1;
+	float	dop;
 	t_tuple	*str;
 	t_tuple	*result;
 
-	str1 = ft_multiply_tuple(normal, 2);
-	str = ft_cross_product(str1, ft_dot_product(in, normal));
+	dop = ft_dot_product(*in, *normal);
+	str = ft_multiply_tuple(normal, 2.0 * dot);
 	result = ft_substract_tuples(in, str);
+	free(scaled_normal);
 	return (result);
 }
 
-t_tuple	*point_light(t_tuple position, t_tuple intensity)
+t_tuple	*point_light(t_tuple *position, t_tuple *intensity)
 {
 	t_light *light;
 
-	light.position = position;
-	light.intensity = intensity;
+	light = malloc(sizeof(t_light);
+
+	light->position = *position;
+	light->intensity = *intensity;
 	return (light);
 }
 
@@ -75,3 +86,4 @@ t_tuple	lighting(t_material mat, t_light light, t_tuple point, t_tuple eyev, t_t
 		
 	}
 }
+
