2a64a3673c58f2e4ffd9493e058986c857b4aabb
Ignacio Churruca Fernandez De Tejada <igchurru@c4r7s2.42urduliz.com>
2025-03-14 13:33:35 +0100

    Added exhaustive comment to tuple_creation.c

--- sources/tuple_creation.c
+++ sources/tuple_creation.c
@@ -6,16 +6,30 @@
 /*   By: igchurru <igchurru@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/20 15:08:59 by igchurru          #+#    #+#             */
-/*   Updated: 2025/03/12 14:09:53 by igchurru         ###   ########.fr       */
+/*   Updated: 2025/03/14 13:33:17 by igchurru         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minirt.h"
 
+/*
+ * ft_create_point - Creates a point in a 3D space using the given
+ *                   coordinates (x, y, z).
+ *
+ * @x: The x-coordinate of the point.
+ * @y: The y-coordinate of the point.
+ * @z: The z-coordinate of the point.
+ *
+ * This function initializes a t_tuple structure with the provided
+ * x, y, and z values. The 'w' component is set to 1, which
+ * indicates that the tuple represents a point (not a vector) in
+ * homogeneous coordinates.
+ *
+ * Return: A t_tuple structure representing a point in 3D space.
+ */
 t_tuple	ft_create_point(float x, float y, float z)
 {
 	t_tuple	new_point;
-
 
 	new_point.x = x;
 	new_point.y = y;
@@ -24,6 +38,21 @@ t_tuple	ft_create_point(float x, float y, float z)
 	return (new_point);
 }
 
+/*
+ * ft_create_vector - Creates a vector in a 3D space using the given
+ *                    components (x, y, z).
+ *
+ * @x: The x-component of the vector.
+ * @y: The y-component of the vector.
+ * @z: The z-component of the vector.
+ *
+ * This function initializes a t_tuple structure with the provided
+ * x, y, and z values. The 'w' component is set to 0, which
+ * indicates that the tuple represents a vector (not a point) in
+ * homogeneous coordinates.
+ *
+ * Return: A t_tuple structure representing a vector in 3D space.
+ */
 t_tuple	ft_create_vector(float x, float y, float z)
 {
 	t_tuple	new_vector;
@@ -35,6 +64,20 @@ t_tuple	ft_create_vector(float x, float y, float z)
 	return (new_vector);
 }
 
+/**
+ * ft_check_equality - 	Compares two t_tuple structures for equality
+ * 						within a small tolerance.
+ * 
+ * This function checks if the x, y, and z components of two given t_tuple 
+ * structures are approximately equal within the defined EPSILON threshold.
+ * This approach accounts for floating-point precision errors.
+ * 
+ * param tuple1: The first tuple to compare.
+ * param tuple2: The second tuple to compare.
+ * 
+ * Return: Returns 0 if the tuples are considered equal within EPSILON,
+ *         otherwise returns 1.
+ */
 int	ft_check_equality(t_tuple tuple1, t_tuple tuple2)
 {
 	if ((fabsf(tuple1.x - tuple2.x) < EPSILON)
