d22372312a2f95d1f061624ca6cfa7a4324b6707
Ignacio Churruca Fernandez De Tejada <igchurru@c4r7s2.42urduliz.com>
2025-03-17 11:27:05 +0100

    Removed testing_folder from following. Also preparing to render own sphere to better follow eandres

--- sources/miniRT.c
+++ sources/miniRT.c
@@ -6,7 +6,7 @@
 /*   By: igchurru <igchurru@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/14 11:09:31 by igchurru          #+#    #+#             */
-/*   Updated: 2025/03/17 10:52:46 by igchurru         ###   ########.fr       */
+/*   Updated: 2025/03/17 11:25:32 by igchurru         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,13 @@ int	ft_minirt(void)
 {
 	mlx_t		*mlx;
 	mlx_image_t	*image;
-	mlx_t		*mlx;
-	mlx_image_t	*image;
 
 	mlx = mlx_init(2400, 1800, "miniRT", true);
 	image = mlx_new_image(mlx, 2400, 1800);
 	//ft_minigun(image);
 	//ft_draw_clock(image);
 	//ft_render_sphere(image);
-	render_lit_sphere(image);
+	//render_lit_sphere(image);
 	mlx_image_to_window(mlx, image, 0, 0);
 	mlx_key_hook(mlx, ft_handle_key, mlx);
 	mlx_loop(mlx);
