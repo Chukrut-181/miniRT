c53cc869ef86d8128b2a0b9f542b9da0e91c9de2
Ignacio Churruca Fernandez De Tejada <igchurru@c4r7s2.42urduliz.com>
2025-05-19 15:50:50 +0200

    Slightly improved main

--- sources/main.c
+++ sources/main.c
@@ -6,7 +6,7 @@
 /*   By: igchurru <igchurru@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/19 11:26:57 by igchurru          #+#    #+#             */
-/*   Updated: 2025/05/19 15:43:32 by igchurru         ###   ########.fr       */
+/*   Updated: 2025/05/19 15:44:40 by igchurru         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -132,7 +132,7 @@ int	main(int argc, char **argv)
 	ft_get_scene(scene, argv[1]);
 //	ft_parsingcheckerprinter(scene);
 	init_mlx(scene);
-	write(1, "Rendering. This may take a while.\n", 13); 
+	write(1, "Rendering\n", 11); 
 	render_scene(scene);
     write(1, "Finished!\n", 9);
 	mlx_put_image_to_window(scene->mlx, scene->win, scene->img.img_ptr, 0, 0);
