04c1e71f781c67ffbe087851335b47f5e2991cc4
Ignacio Churruca Fernandez De Tejada <igchurru@c4r7s2.42urduliz.com>
2025-05-26 14:33:59 +0200

    Added some forgotten files... :oP

---
+++ sources/intersections_utils.c
@@ -0,0 +1,105 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   intersections_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: igchurru <igchurru@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/26 13:26:19 by igchurru          #+#    #+#             */
+/*   Updated: 2025/05/26 13:41:14 by igchurru         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/minirt.h"
+
+bool	quadratic_equation_solution(t_abcd *data, float *t)
+{
+	data->discriminant = data->b * data->b - 4 * data->a * data->c;
+	if (data->discriminant < 0)
+		return (false);
+	t[0] = (-data->b - sqrtf(data->discriminant)) / (2 * data->a);
+	t[1] = (-data->b + sqrtf(data->discriminant)) / (2 * data->a);
+	return (true);
+}
+
+void	update_inter(t_list **inter, t_shape *shape, float time)
+{
+	t_xs	*intersec;
+
+	intersec = malloc(sizeof(t_xs));
+	if (!intersec)
+		return ;
+	intersec->object = shape;
+	intersec->time = time;
+	intersec->hit = 1;
+	intersec->intersec = true;
+	if (*inter == NULL)
+		*inter = ft_lstnew(intersec);
+	else
+		ft_lstadd_back(inter, ft_lstnew(intersec));
+}
+
+void	identify_hit(t_list *xs_list)
+{
+	float	reference;
+	t_xs	*aux;
+	t_xs	*mark;
+
+	reference = 214748368;
+	aux = NULL;
+	mark = NULL;
+	while (xs_list != NULL)
+	{
+		aux = (t_xs *)xs_list->content;
+		if (aux->time < reference && aux->time > 0)
+		{
+			reference = aux->time;
+			if (mark)
+				mark->hit = 0;
+			aux->hit = 1;
+			mark = aux;
+		}
+		xs_list = xs_list->next;
+	}
+}
+
+t_ray	transform(t_ray ray, t_4x4	matrix)
+{
+	t_tuple	multi[2];
+	t_tuple	p;
+	t_tuple	vec;
+	t_ray	new_ray;
+
+	multi[0] = multiply_mat_and_tuple(matrix, ray.origin);
+	multi[1] = multiply_mat_and_tuple(matrix, ray.direction);
+	p = ft_create_point(multi[0].x, multi[0].y, multi[0].z);
+	vec = create_vector(multi[1].x, multi[1].y, multi[1].z);
+	new_ray.origin = p;
+	new_ray.direction = vec;
+	return (new_ray);
+}
+
+t_list	*ft_intersect_world(t_world world, t_ray ray)
+{
+	t_list	*intersections;
+	t_list	*current;
+	t_shape	*shape;
+	bool	found_inter;
+
+	current = world.objects;
+	found_inter = false;
+	intersections = NULL;
+	while (current)
+	{
+		shape = (t_shape *)current->content;
+		if (ft_intersections(ray, shape, &intersections))
+			found_inter = true;
+		current = current->next;
+	}
+	if (found_inter == false)
+	{
+		ft_lstclear(&intersections, free);
+		return (NULL);
+	}
+	return (intersections);
+}
---
+++ sources/light_shading_utils.c
@@ -0,0 +1,60 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   light_shading_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: igchurru <igchurru@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/26 13:31:02 by igchurru          #+#    #+#             */
+/*   Updated: 2025/05/26 13:34:34 by igchurru         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/minirt.h"
+
+t_tuple	normal_at(t_shape *shape, t_tuple point)
+{
+	t_tuple	obj_point;
+	t_tuple	obj_normal;
+	t_4x4	world_normal;
+	t_tuple	normal_at;
+
+	obj_normal = create_vector(0, 0, 0);
+	obj_point = multiply_mat_and_tuple(shape->inverse_matrix, point);
+	if (shape->type == SPHERE)
+		obj_normal = substract_tuples(obj_point, ft_create_point(0, 0, 0));
+	else if (shape->type == PLANE)
+		obj_normal = create_vector(0, 1, 0);
+	else if (shape->type == CYLINDER)
+	{
+		obj_normal = create_vector(obj_point.x, 0, obj_point.z);
+		if (fabsf(obj_point.y - 0) < EPSILON)
+			obj_normal = create_vector(0, -1, 0);
+		else if (fabsf(obj_point.y - 1) < EPSILON)
+			obj_normal = create_vector(0, 1, 0);
+		else
+			obj_normal = create_vector(obj_point.x, 0, obj_point.z);
+	}
+	world_normal = transpose(shape->inverse_matrix);
+	normal_at = multiply_mat_and_tuple(world_normal, obj_normal);
+	return (normalize(normal_at));
+}
+
+t_color	effective_color(t_material m, t_color color, float intensity)
+{
+	t_color	effective_color;
+
+	effective_color.r = m.color.r * color.r * intensity;
+	effective_color.g = m.color.g * color.g * intensity;
+	effective_color.b = m.color.b * color.b * intensity;
+	return (effective_color);
+}
+
+t_light	point_light(t_tuple position, t_color color)
+{
+	t_light	light;
+
+	light.source = position;
+	light.l_color = color;
+	return (light);
+}
